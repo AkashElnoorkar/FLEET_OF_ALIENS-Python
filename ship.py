f2e79664d9cc7e1669f08578f9d2ee186d1d4e74
Akash Elnoorkar <akashelnoorkar@gmail.com>
2020-12-12 11:54:26 +0530

    Game, made using Python

---
+++ ship.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.sprite import Sprite
+class Ship(Sprite):
+    """A class to manage the ship."""
+    def __init__(self,aigame):
+        """Initialize the ship and set it's starting position"""
+        super().__init__()
+        self.screen=aigame.screen
+        self.settings=aigame.settings
+        self.screen_rect=aigame.screen.get_rect()
+        # Load the ship image and get it's rect.
+        self.image=pygame.image.load('ship')
+        self.rect=self.image.get_rect()
+        # Start each new ship at bottom center of the screen,
+        self.rect.midbottom=self.screen_rect.midbottom
+        # Store a decimal value for the ship's horizontal position.
+        self.x=float(self.rect.x)
+        # Movement flag.
+        self.moving_right=False
+        self.moving_left=False
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's 'x' value, not the rect.
+        if self.moving_right and self.rect.right<self.screen_rect.right:
+            self.x+=self.settings.ship_speed
+        if self.moving_left and self.rect.left>0:
+            self.x-=self.settings.ship_speed
+        # Update rect object from 'self.x'
+        self.rect.x=self.x
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image,self.rect)
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom=self.screen_rect.midbottom
+        self.x=float(self.rect.x)
